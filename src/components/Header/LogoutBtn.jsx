6c3d753ec5414de7896f3d668b101f877531b090
Ashikur Rahman <ashikur.jebu111@gmail.com>
2024-04-14 18:01:30 +0600

    upto date

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import {useDispatch} from "react-redux"
-import authService from "../../appwrite/auth.js"
-import {logout } from "../../store/authSlice.js"
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth.js";
+import { logout } from "../../store/authSlice.js";
+import { useNavigate } from "react-router-dom";
 
 function LogoutBtn() {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
-    const lougoutHandler = () => {
+    const navigate = useNavigate();
+
+    const logoutHandler = () => {
+        navigate("/login");
+
         authService.logout().then(() => {
             dispatch(logout());
-        })
-    }
-  return (
-    <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    onClick={lougoutHandler}
-    >Logout</button>
-  )
+        });
+    };
+
+    return (
+        <button
+            className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+            onClick={logoutHandler}
+        >
+            Logout
+        </button>
+    );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
